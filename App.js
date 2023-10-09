f6881ca9444e548bc1bf2da665611bee7496c4b7
JolinZhang <zhangru0026@gmail.com>
2023-10-09 11:25:37 -0500

    Add Home Screen

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <ul>{gradeItem}</ul>
     </View>
   );
 }
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
+const grades = [
+  {id: 0, name:'Prekindergarten'},
+  {id: 1, name:'Kindergarten'},
+  {id: 2, name:'First Grade'},
+  {id: 3, name:'Second Grade'},
+  {id: 4, name:'Third Grade'},
+  {id: 5, name:'Forth Grade'},
+  {id: 6, name:'Fifth Grade'},
+  {id: 7, name:'Sixth Grade'},
+  {id: 8, name:'Seventh Grade'}
+];
+
+const gradeItem = grades.map(grade => <li key={grade.id}>{grade.name}</li>);
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
